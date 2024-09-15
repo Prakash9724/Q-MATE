5cbcec8ccd3447cd5cdf7ef9c588e4d3fea729ad
prakash9724 <hellobrother91069@gmail.com>
2024-09-15 13:41:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='h-screen w-full bg-zinc-900 text-white'>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App
